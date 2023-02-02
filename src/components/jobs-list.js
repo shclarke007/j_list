aea6c73c2ec4f946da08cb41b0c9623e67b2d504
Shaneil Clarke <shaneil.clarke@cutover.com>
2023-02-02 18:58:45 +0000

    added pagination

--- src/components/jobs-list.js
+++ src/components/jobs-list.js
@@ -1,17 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useJobs } from '../main/queries/use-jobs';
 import { Nav } from './nav';
-import { Card, Image, Divider, Grid, Header } from 'semantic-ui-react';
+import { Card, Image, Divider, Grid, Modal, Button, Container} from 'semantic-ui-react';
 import { JobItem } from './job-item';
-import { FavouritesList } from './favourites-list';
+import { Favourites } from './favourites';
 
 /**
  * We're using the useJobs hook to fetch the data from the API, and then we're using the data to render
  * a list of cards
  */
 
-export const JobsList = () => {
-  const { data, isLoading, error, isSuccess } = useJobs()
+export const JobsList = (favourites) => {
+  const [pageNumber, setPageNumber] = useState(1)
+  const { data, isLoading, error, isSuccess } = useJobs(pageNumber)
 
   if (isLoading) {
     return <h2>Loading...</h2>
@@ -25,9 +26,13 @@ export const JobsList = () => {
     <>
       <Nav />
       <Divider />
+      <Modal.Content>
+        { favourites && <Favourites favourites={ favourites } />}
+      </Modal.Content>
+      <Divider />
       <Grid columns={4} container relaxed stackable >
-        { data && isSuccess && data?.length > 0 ?
-          (data?.map((job) => (
+        { data[0] && isSuccess && data[0]?.length > 1 ?
+          (data[0]?.map((job) => (
             <Grid.Column key={ job.id }>
               <Card raised card orange>
                 <Image src={job.attributes.picture.thumb} />
@@ -45,10 +50,17 @@ export const JobsList = () => {
                 <Card.Content extra>
                   <JobItem job={job} />
                 </Card.Content>
-                </Card>
+              </Card>
               </Grid.Column>
           ))) : (<Grid.Column><h2>{'No Jobs listed'}</h2></Grid.Column>)
         }
+        <Container textAlign='center'>
+          <Button primary onClick={() => setPageNumber(pageNumber - 1)} disabled={pageNumber === 1}> Prev
+          </Button>
+          <Button primary onClick={ () => setPageNumber(pageNumber + 1) } disabled={ pageNumber !== data[1]['page-count']}> Next
+          </Button>
+        </Container>
+        
       </Grid>
     </>
   )
--- src/main/queries/use-jobs.js
+++ src/main/queries/use-jobs.js
@@ -1,28 +1,28 @@
 import { useQuery } from 'react-query';
 
-const API_ENDPOINT = 'https://api.teamtailor.com/v1/jobs'
+const API_ENDPOINT = 'https://api.teamtailor.com/v1/jobs?page='
 
 /**
  * `useQuery` is a custom hook that fetches data from the API and returns the data, isLoading, error,
  * and isSuccess states
  */
 
-const fetchJobsListData = async () => {
-  const response =  await fetch(`${API_ENDPOINT}`, {
+const fetchJobsListData = async (pageNumber) => {
+  const response =  await fetch(`${API_ENDPOINT}${pageNumber}`, {
     headers: { 
       'Authorization': `Bearer ${process.env.REACT_APP_API_KEY}`,
       'X-Api-Version': `${process.env.REACT_APP_API_VERSION}`,
     }
   });
   const json = await response.json()
-  return json.data
+  return [json.data, json.meta]
 }
-
-export const useJobs = (data, isLoading, error, isSuccess) => {
-  return useQuery('jobs', fetchJobsListData, {
+export const useJobs = (pageNumber, data, isLoading, error, isSuccess) => {
+  return useQuery(['jobs', pageNumber], () => fetchJobsListData(pageNumber), {
     data,
     isLoading,
     error, 
-    isSuccess }
+    isSuccess,
+    variables: [pageNumber]}
   )
 };
